5d9bf9d9afb86377f99901f7650c50c0446f24b6
Sahad-09 <msahad124@gmail.com>
2024-12-09 01:37:55 +0530

    New memeber form 6

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { Send, MapPin, Phone, Mail, Facebook, Twitter, Instagram } from "lucide-react";
